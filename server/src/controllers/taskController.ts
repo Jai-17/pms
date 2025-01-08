f45ae9e48514bb4c4549e5a601152fd314edce6e
Jai-17 <madhukarjai9@gmail.com>
2025-01-08 22:12:39 +0530

    final

--- server/src/controllers/taskController.ts
+++ server/src/controllers/taskController.ts
@@ -88,3 +88,26 @@ export const updateTaskStatus = async (
       .json({ message: "Error updating task", error: error.message });
   }
 };
+
+export const getUserTasks = async (req: Request, res: Response): Promise<void> => {
+  const { userId } = req.params;
+  try {
+    const tasks = await prisma.task.findMany({
+      where: {
+        OR: [
+          {authorUserId: Number(userId)},
+          {assignedUserId: Number(userId)}
+        ]
+      },
+      include: {
+        author: true,
+        assignee: true,
+      },
+    });
+    res.json(tasks);
+  } catch (error: any) {
+    res
+      .status(500)
+      .json({ message: "Error retrieving user's tasks", error: error.message });
+  }
+};
